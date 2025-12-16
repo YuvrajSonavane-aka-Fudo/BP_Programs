fc908a90f7c29138bb158ca124f8be271349a166
Yuvi <yuvasmail1@gmail.com>
2025-12-16 12:43:47 +0530

    Modification modified:   No3.py

--- No3.py
+++ No3.py
@@ -1,11 +1,13 @@
 def nonPrime(n):
+    if n==0 :
+        return "Neither Prime Nor Composite"
     n = abs(n)
     for i in range(2,n):
         if n%i== 0:
             return "Not Prime"
     return "Prime"
 
-print(nonPrime(23))
+print(nonPrime(-5))
 
 
 
