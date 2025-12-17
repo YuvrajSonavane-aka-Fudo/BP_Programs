9dd3dda9de28e6587ef2a891ada7b6042bfaff95
Yuvi <yuvasmail1@gmail.com>
2025-12-17 11:38:28 +0530

    No24 modified:   No16.py 	new file:   No24.py

--- No16.py
+++ No16.py
@@ -1,10 +1,10 @@
-string1 = "_abc a_der der_ kdfj "
+string1 = "_abc a_Der der_ kdfj "
 
 def underScore(str1):
     list1 = []
     str1 = list(str1.split(" "))
     for i in str1:
-        if "_" in i:
+        if "_" in i and i.islower():
             list1.append(i)
     print(list1)
 
